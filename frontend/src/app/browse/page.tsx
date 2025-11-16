db753bd339f1d32c47ee27f09a6674344defb740
Salah AIT OUBELLI <mohamed-salah-eddine.ait-oubelli@etu.u-bordeaux.fr>
2025-11-16 22:39:15 +0100

    feat: add loading state to browse page

--- frontend/src/app/browse/page.tsx
+++ frontend/src/app/browse/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 
+import { Suspense } from 'react';
 import { BrowsePage } from '@/components/BrowsePage';
 import { Navbar } from '@/components/Navbar';
+
+function BrowseContent() {
+  return <BrowsePage />;
+}
 
 export default function Browse() {
   return (
     <>
       <Navbar />
-      <BrowsePage />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowseContent />
+      </Suspense>
     </>
   );
 }
